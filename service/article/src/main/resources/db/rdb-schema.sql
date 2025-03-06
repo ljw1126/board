5f51baeeefb38f2f0456eaf08c1b3af3b4164312
leejinwoo <leejinwoo1126@gmail.com>
2025-03-06 14:36:08 +0900

    comment rdb-schema.sql 생성

--- service/article/src/main/resources/db/rdb-schema.sql
+++ service/article/src/main/resources/db/rdb-schema.sql
@@ -7,13 +7,3 @@ create table article (
      created_at datetime not null,
      modified_at datetime not null
 );
-
-create table comment (
-     comment_id bigint not null primary key,
-     content varchar(3000) not null,
-     article_id bigint not null,
-     parent_comment_id bigint not null,
-     writer_id bigint not null,
-     deleted bool not null,
-     created_at datetime not null
-);
---
+++ service/comment/src/main/resources/db/rdb-schema.sql
@@ -0,0 +1,19 @@
+create table comment (
+     comment_id bigint not null primary key,
+     content varchar(3000) not null,
+     article_id bigint not null,
+     parent_comment_id bigint not null,
+     writer_id bigint not null,
+     deleted bool not null,
+     created_at datetime not null
+);
+
+create table comment_v2 (
+    comment_id bigint not null primary key,
+    content varchar(3000) not null,
+    article_id bigint not null,
+    writer_id bigint not null,
+    path varchar(25) character set utf8mb4 collate utf8mb4_bin not null,
+    deleted bool not null,
+    created_at datetime not null
+);
